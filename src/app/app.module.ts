36432aa3c40e5538321a22c8d3e5c3309f882da0
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2023-08-08 02:40:11 +0300

    add basic block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
