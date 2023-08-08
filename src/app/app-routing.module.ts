6e6d6c9d1a640e2e45fcbb42995978d0abd7f4fb
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2023-08-08 14:01:37 +0300

    add routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ContentComponent } from './content/content.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MessageComponent } from './message/message.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ContentComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'message', component: MessageComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component: NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MessageComponent } from './message/message.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    ProfileComponent,
+    NotFoundComponent,
+    MessageComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
